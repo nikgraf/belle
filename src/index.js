74076d0502d6dc34f899304c8a8b3a3a12770f16
Nik Graf <nikgraf.com@gmail.com>
2015-03-25 16:32:53 +0800

    chore(components): use upper case for component file names

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@
 
 /* jslint node: true */
 
-import Button from './components/button';
-import Card from './components/card';
-import Input from './components/input';
+import Button from './components/Button';
+import Card from './components/Card';
+import Input from './components/Input';
 
 module.exports = {
   Button: Button,
