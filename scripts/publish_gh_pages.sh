1c7f25f03209050b9252ee7865a012254848ce12
Nik Graf <nikgraf.com@gmail.com>
2015-03-26 15:00:58 +0800

    chore(gh-pages): add script to automatically publish gh-pages with every new version

---
+++ scripts/publish_gh_pages.sh
@@ -0,0 +1,18 @@
+git checkout master
+git checkout -b tmp-gh-pages
+rm .gitignore
+
+cd docs
+npm prune
+npm install
+npm run build
+
+git add js/bundle.js
+git add css/googlecode.css
+git commit -am 'add files'
+cd ..
+git subtree split --prefix docs -b gh-pages
+git push -f origin gh-pages:gh-pages
+git checkout master
+git branch -D tmp-gh-pages
+git branch -D gh-pages
