8c2e35b3a628c7721ab0532fc9a5691900798051
Nik Graf <nikgraf.com@gmail.com>
2015-03-26 09:49:54 +0800

    doc(card): add full-width image example

--- js/CardDocumentation.jsx
+++ js/CardDocumentation.jsx
@@ -12,9 +12,7 @@ export default class CardDocumentation extends Component {
       <h2 id="card" style={ {marginTop: 0, marginBottom: 40} }>Card</h2>
 
       <Card style={{ borderTop: '1px solid #f2f2f2' }}>
-        <div>
-          Add any content here …
-        </div>
+        Add any content here like paragraphs, images or other components …
       </Card>
 
       <i>Note</i>: The card is designed to work on non-white areas. To provide a
@@ -29,13 +27,36 @@ export default class CardDocumentation extends Component {
         <span style={ {color: 'grey'} }> style, id, className, …</span>
       </p>
 
+      <h3>More Examples</h3>
+
+      <p>Card with a full-width image</p>
+
+      <Card style={{ borderTop: '1px solid #f2f2f2' }}>
+        <img src="/images/ngorongoro_crater_small.JPG"
+             width="560"
+             style={{
+               marginLeft: -40,
+               marginRight: -40
+             }}/>
+      </Card>
+
+      <Code value={ imageCodeExample } style={ {marginTop: 40} } />
+
     </Card>;
   }
 }
 
 const basicCodeExample = `<!-- basic card example -->
 <Card style={{ borderTop: '1px solid #f2f2f2' }}>
-  <div>
-    Add any content here …
-  </div>
+  Add any content here like paragraphs, images or other components …
+</Card>`;
+
+const imageCodeExample = `<!-- image card example -->
+<Card style={{ borderTop: '1px solid #f2f2f2' }}>
+  <img src="/images/ngorongoro_crater_small.JPG"
+       width="560"
+       style={{
+         marginLeft: -40,
+         marginRight: -40
+       }}/>
 </Card>`;
