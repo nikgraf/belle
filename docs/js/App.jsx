f1ee492aa3d1a5d254a15c9b44428230fd457cb9
Nik Graf <nikgraf.com@gmail.com>
2015-03-27 09:58:53 +0800

    style(docs): improved spacing

--- docs/js/App.jsx
+++ docs/js/App.jsx
@@ -28,15 +28,15 @@ class App extends Component {
     return <div>
       <div style={ {background: 'rgb(44, 44, 44)', width: '100%'} }>
         <div style={ {margin: '0 auto', width: 640 } }>
-          <h1 style={{ fontSize: 72, margin: 0, paddingTop: 110, color: '#FFF' }}><i style={ belleLogoStyle }></i> Belle</h1>
+          <h1 style={{ fontSize: 72, margin: 0, paddingTop: 110, color: '#FFF', fontFamily: '"Trebuchet MS", Helvetica, sans-serif' }}><i style={ belleLogoStyle }></i> Belle</h1>
 
           <p style={{ fontSize: 18, marginTop: -10, paddingBottom: 60, color: '#B8B8B8'}}>
-            React Components with great User Experience.
+            React Components with great User Experience
           </p>
         </div>
       </div>
 
-      <div style={ {margin: '0 auto', width: 640 } }>
+      <div style={ {margin: '0 auto', width: 640, marginTop: 40 } }>
         <p><b>Notice:</b> Keep in mind this library is early stage and might change a lot soon.</p>
 
         <h2 style={{ float: 'left' }}>Components</h2>
