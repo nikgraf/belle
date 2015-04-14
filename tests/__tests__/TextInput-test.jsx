babc96bc349ce8ac033acfc0f3d610057aad223a
Nik Graf <nikgraf.com@gmail.com>
2015-04-14 09:21:48 +0800

    test(TextInput): add more tests

--- tests/__tests__/TextInput-test.jsx
+++ tests/__tests__/TextInput-test.jsx
@@ -23,6 +23,7 @@ describe('TextInput', () => {
     expect(textareaNode.props.style.overflow).toBe('hidden');
   });
 
+
   it('should be able to adopt the style of the textInput', () => {
     const textInput = TestUtils.renderIntoDocument(
       <TextInput style={{ color: 'red'}} />
@@ -31,6 +32,7 @@ describe('TextInput', () => {
 
     expect(textareaNode.props.style.color).toBe('red');
   });
+
 
   it('should calculate its height after initializing', () => {
     const textInput = TestUtils.renderIntoDocument(
@@ -43,26 +45,114 @@ describe('TextInput', () => {
     expect(textInput._resize.mock.calls.length).toBe(1);
   });
 
-  it('should re-calculate its height after changing', () => {
+
+  it('should re-calculate its height after changing (default)', () => {
     const textInput = TestUtils.renderIntoDocument(
       <TextInput />
     );
 
     textInput._resize = jest.genMockFunction();
-    expect(textInput._resize.mock.calls.length).toBe(0);
 
     const textareaNode = TestUtils.findRenderedDOMComponentWithTag(textInput, 'textarea');
 
     TestUtils.Simulate.change(textareaNode, 'some input text');
 
-    expect(textInput._resize.mock.calls.length).toBe(2);
+    expect(textInput._resize.mock.calls.length).toBe(1);
   });
 
+
+  it('should re-calculate its height after changing (with value & new lines not allowed)', () => {
+    const textInput = TestUtils.renderIntoDocument(
+      <TextInput value="some text"/>
+    );
+
+    textInput._resize = jest.genMockFunction();
+
+    const textareaNode = TestUtils.findRenderedDOMComponentWithTag(textInput, 'textarea');
+
+    TestUtils.Simulate.change(textareaNode, 'some other text');
+
+    expect(textInput._resize.mock.calls.length).toBe(1);
+  });
+
+
+  it('should re-calculate its height after changing (new lines allowed)', () => {
+    const textInput = TestUtils.renderIntoDocument(
+      <TextInput value="some text" allowNewLine={ true }/>
+    );
+
+    textInput._resize = jest.genMockFunction();
+
+    const textareaNode = TestUtils.findRenderedDOMComponentWithTag(textInput, 'textarea');
+
+    TestUtils.Simulate.change(textareaNode, 'some other text');
+
+    expect(textInput._resize.mock.calls.length).toBe(1);
+  });
+
+
+  it('should be able to bind onKeyDown', () => {
+    let wasPressed = false;
+
+    const textInput = TestUtils.renderIntoDocument(
+      <TextInput onKeyDown={ (event) => wasPressed = true }/>
+    );
+
+    const textareaNode = TestUtils.findRenderedDOMComponentWithTag(textInput, 'textarea');
+
+    TestUtils.Simulate.keyDown(textareaNode, {key: "1"});
+
+    expect(wasPressed).toEqual(true);
+  });
+
+
+  it('should be able to bind onChange', () => {
+    let wasChanged = false;
+
+    const textInput = TestUtils.renderIntoDocument(
+      <TextInput onChange={ (event) => wasChanged = true }/>
+    );
+
+    const textareaNode = TestUtils.findRenderedDOMComponentWithTag(textInput, 'textarea');
+
+    TestUtils.Simulate.change(textareaNode, 'some text');
+
+    expect(wasChanged).toEqual(true);
+  });
+
+
+  it('should be able to provide a valueLink', () => {
+    let wasCalled = false;
+
+    const valueLink = {
+      requestChange: () => { wasCalled = true; },
+      value: 'some text'
+    };
+
+    const textInput = TestUtils.renderIntoDocument(
+      <TextInput valueLink={ valueLink }/>
+    );
+
+    const textareaNode = TestUtils.findRenderedDOMComponentWithTag(textInput, 'textarea');
+
+    TestUtils.Simulate.change(textareaNode, 'some other text');
+
+    expect(wasCalled).toEqual(true);
+  });
+
+
+  it('should be able to provide a className', () => {
+    const textInput = TestUtils.renderIntoDocument(
+      <TextInput className="test-me" />
+    );
+
+    const textareaNode = TestUtils.findRenderedDOMComponentWithTag(textInput, 'textarea');
+    expect(textareaNode.props.className.indexOf('test-me')).toBeGreaterThan(-1);
+  });
 
 
   it('should remove the custom styles from the dom when the textInput unmounts', function() {
     injectStyle.removeStyle = jest.genMockFunction();
-    expect(injectStyle.removeStyle.mock.calls.length).toBe(0);
 
     const textInput = TestUtils.renderIntoDocument(
       <TextInput />
