47b6ade53ebd7e876db0c82c4f226bdafa4346d4
Nik Graf <nikgraf.com@gmail.com>
2015-05-16 14:32:09 -0400

    fix(documentation): realign github button

--- docs/js/Base.jsx
+++ docs/js/Base.jsx
@@ -49,7 +49,7 @@ export default class Base extends Component {
             src="https://ghbtns.com/github-btn.html?user=nikgraf&repo=belle&type=star&count=true&size=medium"
             frameBorder="0"
             scrolling="0"
-            width="73px"
+            width="78px"
             height="30px"
             style={ { float: 'right', marginTop: 20 } }>
           </iframe>
