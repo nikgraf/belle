ba736a747b7ee22cc7846ff5935bee65e3187f02
Nik Graf <nikgraf.com@gmail.com>
2015-05-17 11:47:19 -0400

    style(Option): introduce touch-action & user-select

--- src/style/option.js
+++ src/style/option.js
@@ -6,7 +6,13 @@ var optionStyle = {
     boxSizing: 'border-box',
     color: '#666',
     cursor: 'pointer',
-    padding: 10
+    padding: 10,
+    WebkitUserSelect: 'none',
+    MozUserSelect: 'none',
+    MsUserSelect: 'none',
+    userSelect: 'none',
+    MsTouchAction: 'manipulation',
+    touchAction: 'manipulation'
   },
 
   hoverStyle: {
@@ -14,13 +20,25 @@ var optionStyle = {
     background: '#F5F5F5',
     color: '#444',
     cursor: 'pointer',
-    padding: 10
+    padding: 10,
+    WebkitUserSelect: 'none',
+    MozUserSelect: 'none',
+    MsUserSelect: 'none',
+    userSelect: 'none',
+    MsTouchAction: 'manipulation',
+    touchAction: 'manipulation'
   },
 
   selectStyle: {
     boxSizing: 'border-box',
     color: '#666',
-    padding: 0
+    padding: 0,
+    WebkitUserSelect: 'none',
+    MozUserSelect: 'none',
+    MsUserSelect: 'none',
+    userSelect: 'none',
+    MsTouchAction: 'manipulation',
+    touchAction: 'manipulation'
   }
 
 };
