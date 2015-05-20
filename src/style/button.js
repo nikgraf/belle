88a62835a91aaa41713f28b2b01d350eefa06cdb
Nik Graf <nikgraf.com@gmail.com>
2015-05-19 22:04:47 -0400

    fix(Button): prevent flickering for primary button on mobile safari

--- src/style/button.js
+++ src/style/button.js
@@ -86,12 +86,25 @@ var buttonStyle = {
     textAlign: 'center',
     textDecoration: 'none',
     verticalAlign: 'bottom',
+
+    /*
+    To avoid any kind of flickering the user won't get feedback
+    for selecting the button text
+    */
     WebkitUserSelect: 'none',
     MozUserSelect: 'none',
     MsUserSelect: 'none',
     userSelect: 'none',
+
+    /* This button can only be pressed */
     MsTouchAction: 'manipulation',
-    touchAction: 'manipulation'
+    touchAction: 'manipulation',
+
+    /*
+    Prevent flickering while tapping on WebKit
+    http://stackoverflow.com/a/3516243/837709
+    */
+    WebkitTapHighlightColor: 'transparent'
   },
 
   primaryHoverStyle: {
