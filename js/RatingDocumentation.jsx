b6996ecabf018658258b792ea838d55fdac1a59d
jpuri <jyotipuri@gmail.com>
2015-06-02 12:39:25 +0530

    doc(Rating): Changes in docs to add new properties and changes in existing ones

--- js/RatingDocumentation.jsx
+++ js/RatingDocumentation.jsx
@@ -26,10 +26,10 @@ export default class RatingDocumentation extends Component {
             <p style={ {marginTop: 0} }>
               <i>Integer(0-5)</i>
               <br />
-              optional
+              optional (default: 0)
             </p>
             <p>
-              The property specifies the default value for rating.
+              Initial value for rating component.
             </p>
           </td>
         </tr>
@@ -44,7 +44,7 @@ export default class RatingDocumentation extends Component {
               optional
             </p>
             <p>
-              This function will get executed when user changes the rating interactively.
+              Callback executed as user changes rating interactively.
             </p>
           </td>
         </tr>
@@ -56,41 +56,11 @@ export default class RatingDocumentation extends Component {
             <p style={ {marginTop: 0} }>
               <i>Character</i>
               <br />
-              optional
+              optional (default: '★')
               </p>
               <p>
-                This character will be used as the rating character in the component.
+                Rating character used in the component.
               </p>
-          </td>
-        </tr>
-        <tr>
-          <td style={ propertyNameStyle }>
-            style
-          </td>
-          <td style={ propertyDescriptionStyle }>
-            <p style={ {marginTop: 0} }>
-              <i>Object</i>
-              <br />
-              optional
-            </p>
-            <p>
-              These are general css styles that will get applied to the Rating component.
-            </p>
-          </td>
-        </tr>
-        <tr>
-          <td style={ propertyNameStyle }>
-            tabIndex
-          </td>
-          <td style={ propertyDescriptionStyle }>
-            <p style={ {marginTop: 0} }>
-              <i>Integer</i>
-              <br />
-              optional
-            </p>
-            <p>
-              This tabIndex will get assigned to the Rating component on the page.
-            </p>
           </td>
         </tr>
         <tr>
@@ -101,254 +71,34 @@ export default class RatingDocumentation extends Component {
             <p style={ {marginTop: 0} }>
               <i>Boolean</i>
               <br />
-              optional
+              optional (default: false)
             </p>
             <p>
-              Property can be used to prevent any user input and changes to rating.
+              Can be used to disable rating component.
             </p>
           </td>
         </tr>
         <tr>
           <td style={ propertyNameStyle }>
-            className
+            preventFocusStyleForTouchAndClick
           </td>
           <td style={ propertyDescriptionStyle }>
             <p style={ {marginTop: 0} }>
-              <i>String</i>
+              <i>Boolean</i>
               <br />
-              optional
+              optional (default: true)
             </p>
             <p>
-              Name of the classes that will get added to the component.
-            </p>
-          </td>
-        </tr>
-        <tr>
-          <td style={ propertyNameStyle }>
-            hoverStyle
-          </td>
-          <td style={ propertyDescriptionStyle }>
-            <p style={ {marginTop: 0} }>
-              <i>Object</i>
-              <br />
-              optional
-            </p>
-            <p>
-              This style will be applied to the component when it is hovered.
-            </p>
-          </td>
-        </tr>
-        <tr>
-          <td style={ propertyNameStyle }>
-            focusStyle
-          </td>
-          <td style={ propertyDescriptionStyle }>
-            <p style={ {marginTop: 0} }>
-              <i>Object</i>
-              <br />
-              optional
-            </p>
-            <p>
-              This style will be applied to the component when it is focused.
-            </p>
-          </td>
-        </tr>
-        <tr>
-          <td style={ propertyNameStyle }>
-            disabledStyle
-          </td>
-          <td style={ propertyDescriptionStyle }>
-            <p style={ {marginTop: 0} }>
-              <i>Object</i>
-              <br />
-              optional
-            </p>
-            <p>
-              This style will be applied to the component if it is disabled.
-            </p>
-          </td>
-        </tr>
-        <tr>
-          <td style={ propertyNameStyle }>
-            disabledHoverStyle
-          </td>
-          <td style={ propertyDescriptionStyle }>
-            <p style={ {marginTop: 0} }>
-              <i>Object</i>
-              <br />
-              optional
-            </p>
-            <p>
-              This style will be applied to the component if it is disabled and it is hovered.
-            </p>
-          </td>
-        </tr>
-        <tr>
-          <td style={ propertyNameStyle }>
-            onMouseEnter
-          </td>
-          <td style={ propertyDescriptionStyle }>
-            <p style={ {marginTop: 0} }>
-              <i>Function</i>
-              <br />
-              optional
-            </p>
-            <p>
-              This callback will get executed as mouse enters into the component.
-            </p>
-          </td>
-        </tr>
-        <tr>
-          <td style={ propertyNameStyle }>
-            onMouseLeave
-          </td>
-          <td style={ propertyDescriptionStyle }>
-            <p style={ {marginTop: 0} }>
-              <i>Function</i>
-              <br />
-              optional
-            </p>
-            <p>
-              This callback will get executed as mouse leaves the component.
-            </p>
-          </td>
-        </tr>
-        <tr>
-          <td style={ propertyNameStyle }>
-            onMouseMove
-          </td>
-          <td style={ propertyDescriptionStyle }>
-            <p style={ {marginTop: 0} }>
-              <i>Function</i>
-              <br />
-              optional
-            </p>
-            <p>
-              This callback will get executed as mouse moves over the component.
-            </p>
-          </td>
-        </tr>
-        <tr>
-          <td style={ propertyNameStyle }>
-            onTouchStart
-          </td>
-          <td style={ propertyDescriptionStyle }>
-            <p style={ {marginTop: 0} }>
-              <i>Function</i>
-              <br />
-              optional
-            </p>
-            <p>
-              This callback will get executed as on touch start event.
-            </p>
-          </td>
-        </tr>
-        <tr>
-          <td style={ propertyNameStyle }>
-            onTouchMove
-          </td>
-          <td style={ propertyDescriptionStyle }>
-            <p style={ {marginTop: 0} }>
-              <i>Function</i>
-              <br />
-              optional
-            </p>
-            <p>
-              This callback will get executed as on touch move event.
-            </p>
-          </td>
-        </tr>
-        <tr>
-          <td style={ propertyNameStyle }>
-            onTouchEnd
-          </td>
-          <td style={ propertyDescriptionStyle }>
-            <p style={ {marginTop: 0} }>
-              <i>Function</i>
-              <br />
-              optional
-            </p>
-            <p>
-              This callback will get executed as on touch end event.
-            </p>
-          </td>
-        </tr>
-        <tr>
-          <td style={ propertyNameStyle }>
-            onTouchCancel
-          </td>
-          <td style={ propertyDescriptionStyle }>
-            <p style={ {marginTop: 0} }>
-              <i>Function</i>
-              <br />
-              optional
-            </p>
-            <p>
-              This callback will get executed as on touch cancel event.
-            </p>
-          </td>
-        </tr>
-        <tr>
-          <td style={ propertyNameStyle }>
-            onFocus
-          </td>
-          <td style={ propertyDescriptionStyle }>
-            <p style={ {marginTop: 0} }>
-              <i>Function</i>
-              <br />
-              optional
-            </p>
-            <p>
-              This callback will get executed as the component receives focus.
-            </p>
-          </td>
-        </tr>
-        <tr>
-          <td style={ propertyNameStyle }>
-            onBlur
-          </td>
-          <td style={ propertyDescriptionStyle }>
-            <p style={ {marginTop: 0} }>
-              <i>Function</i>
-              <br />
-              optional
-            </p>
-            <p>
-              This callback will get executed as focus leaves the component.
-            </p>
-          </td>
-        </tr>
-        <tr>
-          <td style={ propertyNameStyle }>
-            onClick
-          </td>
-          <td style={ propertyDescriptionStyle }>
-            <p style={ {marginTop: 0} }>
-              <i>Function</i>
-              <br />
-              optional
-            </p>
-            <p>
-              This callback will get executed as mouse is clicked over the component.
-            </p>
-          </td>
-        </tr>
-        <tr>
-          <td style={ propertyNameStyle }>
-            onKeyDown
-          </td>
-          <td style={ propertyDescriptionStyle }>
-            <p style={ {marginTop: 0} }>
-              <i>Function</i>
-              <br />
-              optional
-            </p>
-            <p>
-              This callback will get executed on key press if the component is focused.
+              Prevents the focus style being applied in case the buttons becomes focused by a click or touch..
             </p>
           </td>
         </tr>
       </table>
+
+      <p>
+      Other supported properties include:
+      <span style={ {color: 'grey'} }> tabIndex, style, className, hoverStyle, focusStyle, disabledStyle, disabledHoverStyle, onMouseDown, onMouseUp, onMouseEnter, onMouseMove, onMouseLeave, onTouchStart, onTouchMove, onTouchEnd, onTouchCancel, onFocus, onBlur, onClick, onKeyDown, …</span>
+      </p>
 
       <h3>More Examples</h3>
 
