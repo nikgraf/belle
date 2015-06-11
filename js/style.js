2c3ea873794d5cebd06d3917a4dcb47cad14fd5d
Nik Graf <nikgraf.com@gmail.com>
2015-06-11 16:43:36 +0200

    doc(Rating): style properties documentations

---
+++ js/style.js
@@ -0,0 +1,21 @@
+"use strict";
+
+var style = {
+
+  propertyNameStyle: {
+    padding: '0 20px 0 0',
+    textAlign: 'left',
+    verticalAlign: 'top',
+    color: 'grey'
+  },
+
+  propertyDescriptionStyle: {
+    padding: 0,
+    textAlign: 'left',
+    verticalAlign: 'top',
+    paddingBottom: 30
+  }
+
+};
+
+export default style;
