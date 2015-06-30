e4f070c9287d394ea18e58aa1b56f51df8b9c305
Nik Graf <nikgraf.com@gmail.com>
2015-06-30 02:27:22 +0200

    doc(Toggle): add properties documentation

--- docs/js/ToggleDocumentation.jsx
+++ docs/js/ToggleDocumentation.jsx
@@ -74,11 +74,288 @@ export default class ToggleDocumentation extends Component {
               </p>
             </td>
           </tr>
+
+          <tr>
+            <td style={ propertyNameStyle }>
+              activeHandleStyle
+            </td>
+          </tr>
+          <tr>
+            <td style={ propertyDescriptionStyle }>
+              <p>
+                <i>Object</i>
+                <br />
+                optional
+              </p>
+              <p>
+                Works like React's built-in style property.
+                Becomes active once the user clicks or touches the toggle (including the check & cross areas).
+              </p>
+            </td>
+          </tr>
+
+          <tr>
+            <td style={ propertyNameStyle }>
+              checkAreaStyle
+            </td>
+          </tr>
+          <tr>
+            <td style={ propertyDescriptionStyle }>
+              <p>
+                <i>Object</i>
+                <br />
+                optional
+              </p>
+              <p>
+                Works like React's built-in style property.
+              </p>
+            </td>
+          </tr>
+
+          <tr>
+            <td style={ propertyNameStyle }>
+              crossAreaStyle
+            </td>
+          </tr>
+          <tr>
+            <td style={ propertyDescriptionStyle }>
+              <p>
+                <i>Object</i>
+                <br />
+                optional
+              </p>
+              <p>
+                Works like React's built-in style property.
+              </p>
+            </td>
+          </tr>
+
+          <tr>
+            <td style={ propertyNameStyle }>
+              disabledStyle
+            </td>
+          </tr>
+          <tr>
+            <td style={ propertyDescriptionStyle }>
+              <p>
+                <i>Object</i>
+                <br />
+                optional
+              </p>
+              <p>
+                Works like React's built-in style property. Becomes active once the Toggle
+                is disabled via the disabled property.
+              </p>
+            </td>
+          </tr>
+
+          <tr>
+            <td style={ propertyNameStyle }>
+              disabledHandleStyle
+            </td>
+          </tr>
+          <tr>
+            <td style={ propertyDescriptionStyle }>
+              <p>
+                <i>Object</i>
+                <br />
+                optional
+              </p>
+              <p>
+                Works like React's built-in style property. Becomes active once the Toggle
+                is disabled via the disabled property.
+              </p>
+            </td>
+          </tr>
+
+          <tr>
+            <td style={ propertyNameStyle }>
+              focusStyle
+            </td>
+          </tr>
+          <tr>
+            <td style={ propertyDescriptionStyle }>
+              <p>
+                <i>Object</i>
+                <br />
+                optional
+              </p>
+              <p>
+                Works like React's built-in style property. Becomes active once the Toggle
+                is focused on. Only applies to the handle node.
+              </p>
+            </td>
+          </tr>
+
+          <tr>
+            <td style={ propertyNameStyle }>
+              handleStyle
+            </td>
+          </tr>
+          <tr>
+            <td style={ propertyDescriptionStyle }>
+              <p>
+                <i>Object</i>
+                <br />
+                optional
+              </p>
+              <p>
+                Works like React's built-in style property. Only applies to the handle node.
+              </p>
+            </td>
+          </tr>
+
+          <tr>
+            <td style={ propertyNameStyle }>
+              hoverHandleStyle
+            </td>
+          </tr>
+          <tr>
+            <td style={ propertyDescriptionStyle }>
+              <p>
+                <i>Object</i>
+                <br />
+                optional
+              </p>
+              <p>
+                Works like React's built-in style property. Becomes active once a user moves the mouse above the Toggle component. Only applies to the handle node.
+              </p>
+            </td>
+          </tr>
+
+          <tr>
+            <td style={ propertyNameStyle }>
+              sliderStyle
+            </td>
+          </tr>
+          <tr>
+            <td style={ propertyDescriptionStyle }>
+              <p>
+                <i>Object</i>
+                <br />
+                optional
+              </p>
+              <p>
+                Works like React's built-in style property. Only applies to the slider node.
+              </p>
+            </td>
+          </tr>
+
+          <tr>
+            <td style={ propertyNameStyle }>
+              disabled
+            </td>
+          </tr>
+          <tr>
+            <td style={ propertyDescriptionStyle }>
+              <p>
+                <i>Boolean</i>
+                <br />
+                default: false</p>
+              <p>If true the Toggle will be disabled and can't be changed by the user.</p>
+            </td>
+          </tr>
+
+          <tr>
+            <td style={ propertyNameStyle }>
+              checkAreaProps
+            </td>
+          </tr>
+          <tr>
+            <td style={ propertyDescriptionStyle }>
+              <p>
+                <i>Object</i>
+                <br />
+                optional
+              </p>
+              <p>
+                This object allows to provide any kind of valid properties for a
+                div tag.
+              </p>
+            </td>
+          </tr>
+
+          <tr>
+            <td style={ propertyNameStyle }>
+              crossAreaProps
+            </td>
+          </tr>
+          <tr>
+            <td style={ propertyDescriptionStyle }>
+              <p>
+                <i>Object</i>
+                <br />
+                optional
+              </p>
+              <p>
+                This object allows to provide any kind of valid properties for a
+                div tag.
+              </p>
+            </td>
+          </tr>
+
+          <tr>
+            <td style={ propertyNameStyle }>
+              handleProps
+            </td>
+          </tr>
+          <tr>
+            <td style={ propertyDescriptionStyle }>
+              <p>
+                <i>Object</i>
+                <br />
+                optional
+              </p>
+              <p>
+                This object allows to provide any kind of valid properties for a
+                div tag.
+              </p>
+            </td>
+          </tr>
+
+          <tr>
+            <td style={ propertyNameStyle }>
+              sliderProps
+            </td>
+          </tr>
+          <tr>
+            <td style={ propertyDescriptionStyle }>
+              <p>
+                <i>Object</i>
+                <br />
+                optional
+              </p>
+              <p>
+                This object allows to provide any kind of valid properties for a
+                div tag.
+              </p>
+            </td>
+          </tr>
+
+          <tr>
+            <td style={ propertyNameStyle }>
+              sliderWrapperProps
+            </td>
+          </tr>
+          <tr>
+            <td style={ propertyDescriptionStyle }>
+              <p>
+                <i>Object</i>
+                <br />
+                optional
+              </p>
+              <p>
+                This object allows to provide any kind of valid properties for a
+                div tag.
+              </p>
+            </td>
+          </tr>
+
         </table>
 
         <p>
           Any property valid for a HTML div like
-          <span style={ {color: 'grey'} }> style, id, className, …</span>
+          <span style={ {color: 'grey'} }> style, id, className, onMouseDown, onTouchStart …</span>
         </p>
 
         <h3>More Examples</h3>
