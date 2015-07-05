4eca5a695d90c1b038eafdf85e2fb3c77f870295
Nik Graf <nikgraf.com@gmail.com>
2015-07-06 00:55:35 +0200

    refactor(Option, Placeholder): align with eslint rules

--- src/components/Option.jsx
+++ src/components/Option.jsx
@@ -1,8 +1,22 @@
-"use strict";
-
 import React, {Component} from 'react';
 import {extend, omit} from 'underscore';
 import style from '../style/option';
+
+/**
+ * Returns an object with properties that are relevant for the wrapping div.
+ */
+function sanitizeChildProperties(properties) {
+  return omit(properties, [
+    'style',
+    'hoverStyle',
+    'selectStyle',
+    'disabledSelectStyle',
+    'value',
+    '_isHovered',
+    '_isDisplayedAsSelected',
+    '_isDisabled'
+  ]);
+}
 
 /**
  * Option component.
@@ -11,7 +25,7 @@ import style from '../style/option';
  */
 export default class Option extends Component {
 
-  constructor (properties) {
+  constructor(properties) {
     super(properties);
     this.state = {
       childProperties: sanitizeChildProperties(properties)
@@ -22,11 +36,11 @@ export default class Option extends Component {
    * Update the childProperties based on the updated properties passed to the
    * Option.
    */
-  componentWillReceiveProps (properties) {
+  componentWillReceiveProps(properties) {
     this.setState({ childProperties: sanitizeChildProperties(properties) });
   }
 
-  render () {
+  render() {
     const defaultStyle = extend({}, style.style, this.props.style);
     const hoverStyle = extend({}, style.hoverStyle, this.props.hoverStyle);
     const selectStyle = extend({}, style.selectStyle, this.props.selectStyle);
@@ -56,10 +70,15 @@ export default class Option extends Component {
 }
 
 Option.propTypes = {
+  children: React.PropTypes.oneOfType([
+    React.PropTypes.arrayOf(React.PropTypes.node),
+    React.PropTypes.node
+  ]),
   style: React.PropTypes.object,
   hoverStyle: React.PropTypes.object,
   selectStyle: React.PropTypes.object,
   disabledSelectStyle: React.PropTypes.object,
+  _isDisabled: React.PropTypes.bool,
   _isHovered: React.PropTypes.bool,
   _isDisplayedAsSelected: React.PropTypes.bool,
   value: React.PropTypes.oneOfType([
@@ -76,19 +95,3 @@ Option.defaultProps = {
   _isDisplayedAsSelected: false,
   _isDisabled: false
 };
-
-/**
- * Returns an object with properties that are relevant for the wrapping div.
- */
-function sanitizeChildProperties(properties) {
-  return omit(properties, [
-    'style',
-    'hoverStyle',
-    'selectStyle',
-    'disabledSelectStyle',
-    'value',
-    '_isHovered',
-    '_isDisplayedAsSelected',
-    '_isDisabled'
-  ]);
-}
