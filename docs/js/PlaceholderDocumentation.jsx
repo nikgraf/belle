03a5c9cead476bf1a12585c9f40907e9a28809bf
jpuri <jyotipuri@gmail.com>
2015-07-08 15:55:12 +0530

    docs: code changes to align to ESLint rules

--- docs/js/PlaceholderDocumentation.jsx
+++ docs/js/PlaceholderDocumentation.jsx
@@ -1,12 +1,18 @@
 import React, {Component} from 'react';
 import {Option, Placeholder, Select} from 'belle';
-import {map} from 'underscore';
 import Code from './Code';
+
+const basicCodeExample = `<!-- basic select example with a placeholder -->
+<Select>
+  <Placeholder>Choose a City</Placeholder>
+  <Option value="tokyo">Tokyo</Option>
+  <Option value="vienna">Vienna</Option>
+</Select>`;
 
 export default class PlaceholderDocumentation extends Component {
 
   render() {
-    return <div>
+    return (<div>
 
       <h2 style={ {marginTop: 0, marginBottom: 40} }>Placeholder</h2>
 
@@ -25,13 +31,6 @@ export default class PlaceholderDocumentation extends Component {
         <span style={ {color: 'grey'} }> style, id, className, …</span>
       </p>
 
-    </div>;
+    </div>);
   }
 }
-
-const basicCodeExample = `<!-- basic select example with a placeholder -->
-<Select>
-  <Placeholder>Choose a City</Placeholder>
-  <Option value="tokyo">Tokyo</Option>
-  <Option value="vienna">Vienna</Option>
-</Select>`;
--- docs/js/SelectDocumentation.jsx
+++ docs/js/SelectDocumentation.jsx
@@ -1,21 +1,176 @@
 import React, {Component} from 'react';
 import {Option, Placeholder, Select, Separator, TextInput} from 'belle';
-import {map} from 'underscore';
 import Code from './Code';
 import {propertyNameStyle, propertyDescriptionStyle} from './style';
 
+function positionOptions(selectComponent) {
+  const menuNode = React.findDOMNode(selectComponent.refs.menu);
+  menuNode.style.top = '35px';
+}
+
 const fruits = [
+  { value: 'pineapple', content: (<span>🍍 Pineapple</span>) },
+  { value: 'banana', content: (<span>🍌 Banana</span>) },
+  { value: 'peach', content: (<span>🍑 Peach</span>) },
+  { value: 'pear', content: (<span>🍐 Pear</span>) },
+  { value: 'cherries', content: (<span>🍒 Cherries</span>) }
+];
+
+const basicCodeExample = `<!-- basic select example -->
+<Select>
+  <Placeholder>Choose a City</Placeholder>
+  <Option value="berlin">Berlin</Option>
+  <Option value="tokyo">Tokyo</Option>
+  <Option value="vienna">Vienna</Option>
+</Select>`;
+
+const dataCodeExamplePartOne = `<!-- defining the data -->
+var fruits = [
   { value: "pineapple", content: (<span>🍍 Pineapple</span>) },
   { value: "banana", content: (<span>🍌 Banana</span>) },
   { value: "peach", content: (<span>🍑 Peach</span>) },
   { value: "pear", content: (<span>🍐 Pear</span>) },
   { value: "cherries", content: (<span>🍒 Cherries</span>) }
-];
+];`;
+
+const dataCodeExamplePartTwo = `<!-- filling a select with Option  -->
+<Select defaultValue={ fruits[3].value }
+        onUpdate={ function(event) { console.log(event.value); } }>
+  {
+    fruits.map(function(fruit, index) {
+      return (
+        <Option value={ fruit.value }
+                key={ index }>
+          { fruit.content }
+        </Option>
+      );
+    })
+  }
+</Select>`;
+
+const formCodeExample = `<!-- form consiting of an input & a select  -->
+<div style={ { display: 'table' } }>
+  <TextInput style={ { width: 138,
+                       float: 'left'} }
+             placeholder="Fill in your address …" />
+           <div style={ { width: 110,
+                          float: 'left',
+                          marginLeft: 16 } }>
+    <Select defaultValue="tokyo"
+            menuStyle={ { height: 160,
+                                 overflow: 'scroll' } }>
+      <Option value="berlin">Berlin</Option>
+      <Option value="hong-kong">Hong Kong</Option>
+      <Option value="istanbul">Istanbul</Option>
+      <Option value="rome">Rome</Option>
+      <Option value="san-francisco">San Francisco</Option>
+      <Option value="tokyo">Tokyo</Option>
+      <Option value="vienna">Vienna</Option>
+    </Select>
+  </div>
+</div>`;
+
+const separatorCodeExample = `<!-- basic select example with separators -->
+<Select>
+  <Separator>Asia</Separator>
+  <Option value="hong-kong">Hong Kong</Option>
+  <Option value="tokyo">Tokyo</Option>
+  <Separator>Europe</Separator>
+  <Option value="berlin">Berlin</Option>
+  <Option value="istanbul">Istanbul</Option>
+</Select>`;
+
+const advancedStylingCodeExample = `<!-- select example with more advanced styling -->
+<Select menuStyle={{ padding: 6 }}>
+  <Placeholder>Choose your next Vacation</Placeholder>
+  <Option value="santorini"
+          style={{
+            padding: '15px 0 0 60px',
+            height: 50,
+            background: 'url(images/santorini_100.jpg) no-repeat',
+            backgroundSize: '50px 50px',
+            backgroundColor: '#FFEE82'
+          }}
+          hoverStyle={{
+            padding: '15px 0 0 60px',
+            height: 50,
+            background: 'url(images/santorini_100.jpg) no-repeat',
+            backgroundSize: '50px 50px',
+            backgroundColor: '#FFE95D'
+          }} >
+    Santorini (Special Deal)
+  </Option>
+  <Separator style={{ height: 4, padding: 0 }}></Separator>
+  <Option value="yosemite"
+          style={{
+            padding: '15px 0 0 60px',
+            height: 50,
+            background: 'url(images/yosemite_100.jpg) no-repeat',
+            backgroundSize: '50px 50px'
+          }}
+          hoverStyle={{
+            padding: '15px 0 0 60px',
+            height: 50,
+            backgroundColor: '#F5F5F5',
+            background: 'url(images/yosemite_100.jpg) no-repeat',
+            backgroundSize: '50px 50px'
+          }} >
+    Yosemite
+  </Option>
+  <Separator style={{ height: 4, padding: 0 }}></Separator>
+  <Option value="croatia"
+          style={{
+            padding: '15px 0 0 60px',
+            height: 50,
+            background: 'url(images/croatia_100.jpg) no-repeat',
+            backgroundSize: '50px 50px'
+          }}
+          hoverStyle={{
+            padding: '15px 0 0 60px',
+            height: 50,
+            backgroundColor: '#F5F5F5',
+            background: 'url(images/croatia_100.jpg) no-repeat',
+            backgroundSize: '50px 50px'
+          }} >
+    Croatia
+  </Option>
+</Select>`;
+
+const positionOptionsCodeExample = `<!-- custom positionOptions function in your JS code -->
+function positionOptions (selectComponent) {
+  const menuNode = React.findDOMNode(selectComponent.refs.menu);
+  menuNode.style.top = '35px';
+}`;
+
+const positionOptionsSelectCodeExample = `<!-- select with a custom positionOptions function -->
+<Select positionOptions={ positionOptions }>
+  <Placeholder>Choose a City</Placeholder>
+  <Option value="berlin">Berlin</Option>
+  <Option value="tokyo">Tokyo</Option>
+  <Option value="vienna">Vienna</Option>
+</Select>`;
+
+const htmlStructure = `<div tabIndex="0"
+     style={ wrapperStyle }>
+  <div style={ style }>
+    <Option /> or <Placeholder />
+    <span style={ caretToCloseStyle or caretToOpenStyle } />
+  </div>
+  <ul style={ menuStyle }>
+    <li>
+      <Option /> or <Separator />
+    </li>
+    <li>
+      <Option /> or <Separator />
+    </li>
+    … more entries …
+  </ul>
+</div>`;
 
 export default class SelectDocumentation extends Component {
 
   render() {
-    return <div>
+    return (<div>
 
       <h2 style={ {marginTop: 0, marginBottom: 40} }>Select</h2>
 
@@ -419,7 +574,7 @@ export default class SelectDocumentation extends Component {
       <p>Select from a dynamic data set including a defaultValue & onUpdate callback</p>
 
       <Select defaultValue={ fruits[3].value }
-              onUpdate={ function(event) { console.log(event.value); } }>
+              onUpdate={ (event) => { console.log(event.value); } }>
         {
           fruits.map(function(fruit, index) {
             return (
@@ -440,10 +595,10 @@ export default class SelectDocumentation extends Component {
 
       <div style={ { display: 'table' } }>
         <TextInput style={ { width: 138,
-                             float: 'left'} }
+                             'float': 'left'} }
                    placeholder="Fill in your address …" />
                  <div style={ { width: 110,
-                                float: 'left',
+                                'float': 'left',
                                 marginLeft: 16 } }>
           <Select defaultValue="tokyo"
                   menuStyle={ { height: 160,
@@ -495,7 +650,7 @@ export default class SelectDocumentation extends Component {
                 }} >
           Santorini (Special Deal)
         </Option>
-        <Separator style={{ height: 4, padding: 0 }}></Separator>
+        <Separator style={{ height: 4, padding: 0 }}/>
         <Option value="yosemite"
                 style={{
                   padding: '15px 0 0 60px',
@@ -512,7 +667,7 @@ export default class SelectDocumentation extends Component {
                 }} >
           Yosemite
         </Option>
-        <Separator style={{ height: 4, padding: 0 }}></Separator>
+        <Separator style={{ height: 4, padding: 0 }}/>
         <Option value="croatia"
                 style={{
                   padding: '15px 0 0 60px',
@@ -546,162 +701,6 @@ export default class SelectDocumentation extends Component {
 
       <Code value={ positionOptionsSelectCodeExample } style={ {marginTop: 40} } />
 
-    </div>;
+    </div>);
   }
 }
-
-function positionOptions (selectComponent) {
-  const menuNode = React.findDOMNode(selectComponent.refs.menu);
-  menuNode.style.top = '35px';
-}
-
-const basicCodeExample = `<!-- basic select example -->
-<Select>
-  <Placeholder>Choose a City</Placeholder>
-  <Option value="berlin">Berlin</Option>
-  <Option value="tokyo">Tokyo</Option>
-  <Option value="vienna">Vienna</Option>
-</Select>`;
-
-const dataCodeExamplePartOne = `<!-- defining the data -->
-var fruits = [
-  { value: "pineapple", content: (<span>🍍 Pineapple</span>) },
-  { value: "banana", content: (<span>🍌 Banana</span>) },
-  { value: "peach", content: (<span>🍑 Peach</span>) },
-  { value: "pear", content: (<span>🍐 Pear</span>) },
-  { value: "cherries", content: (<span>🍒 Cherries</span>) }
-];`;
-
-const dataCodeExamplePartTwo = `<!-- filling a select with Option  -->
-<Select defaultValue={ fruits[3].value }
-        onUpdate={ function(event) { console.log(event.value); } }>
-  {
-    fruits.map(function(fruit, index) {
-      return (
-        <Option value={ fruit.value }
-                key={ index }>
-          { fruit.content }
-        </Option>
-      );
-    })
-  }
-</Select>`;
-
-const formCodeExample = `<!-- form consiting of an input & a select  -->
-<div style={ { display: 'table' } }>
-  <TextInput style={ { width: 138,
-                       float: 'left'} }
-             placeholder="Fill in your address …" />
-           <div style={ { width: 110,
-                          float: 'left',
-                          marginLeft: 16 } }>
-    <Select defaultValue="tokyo"
-            menuStyle={ { height: 160,
-                                 overflow: 'scroll' } }>
-      <Option value="berlin">Berlin</Option>
-      <Option value="hong-kong">Hong Kong</Option>
-      <Option value="istanbul">Istanbul</Option>
-      <Option value="rome">Rome</Option>
-      <Option value="san-francisco">San Francisco</Option>
-      <Option value="tokyo">Tokyo</Option>
-      <Option value="vienna">Vienna</Option>
-    </Select>
-  </div>
-</div>`;
-
-const separatorCodeExample = `<!-- basic select example with separators -->
-<Select>
-  <Separator>Asia</Separator>
-  <Option value="hong-kong">Hong Kong</Option>
-  <Option value="tokyo">Tokyo</Option>
-  <Separator>Europe</Separator>
-  <Option value="berlin">Berlin</Option>
-  <Option value="istanbul">Istanbul</Option>
-</Select>`;
-
-const advancedStylingCodeExample = `<!-- select example with more advanced styling -->
-<Select menuStyle={{ padding: 6 }}>
-  <Placeholder>Choose your next Vacation</Placeholder>
-  <Option value="santorini"
-          style={{
-            padding: '15px 0 0 60px',
-            height: 50,
-            background: 'url(images/santorini_100.jpg) no-repeat',
-            backgroundSize: '50px 50px',
-            backgroundColor: '#FFEE82'
-          }}
-          hoverStyle={{
-            padding: '15px 0 0 60px',
-            height: 50,
-            background: 'url(images/santorini_100.jpg) no-repeat',
-            backgroundSize: '50px 50px',
-            backgroundColor: '#FFE95D'
-          }} >
-    Santorini (Special Deal)
-  </Option>
-  <Separator style={{ height: 4, padding: 0 }}></Separator>
-  <Option value="yosemite"
-          style={{
-            padding: '15px 0 0 60px',
-            height: 50,
-            background: 'url(images/yosemite_100.jpg) no-repeat',
-            backgroundSize: '50px 50px'
-          }}
-          hoverStyle={{
-            padding: '15px 0 0 60px',
-            height: 50,
-            backgroundColor: '#F5F5F5',
-            background: 'url(images/yosemite_100.jpg) no-repeat',
-            backgroundSize: '50px 50px'
-          }} >
-    Yosemite
-  </Option>
-  <Separator style={{ height: 4, padding: 0 }}></Separator>
-  <Option value="croatia"
-          style={{
-            padding: '15px 0 0 60px',
-            height: 50,
-            background: 'url(images/croatia_100.jpg) no-repeat',
-            backgroundSize: '50px 50px'
-          }}
-          hoverStyle={{
-            padding: '15px 0 0 60px',
-            height: 50,
-            backgroundColor: '#F5F5F5',
-            background: 'url(images/croatia_100.jpg) no-repeat',
-            backgroundSize: '50px 50px'
-          }} >
-    Croatia
-  </Option>
-</Select>`;
-
-const positionOptionsCodeExample = `<!-- custom positionOptions function in your JS code -->
-function positionOptions (selectComponent) {
-  const menuNode = React.findDOMNode(selectComponent.refs.menu);
-  menuNode.style.top = '35px';
-}`;
-
-const positionOptionsSelectCodeExample = `<!-- select with a custom positionOptions function -->
-<Select positionOptions={ positionOptions }>
-  <Placeholder>Choose a City</Placeholder>
-  <Option value="berlin">Berlin</Option>
-  <Option value="tokyo">Tokyo</Option>
-  <Option value="vienna">Vienna</Option>
-</Select>`;
-
-const htmlStructure = `<div tabIndex="0"
-     style={ wrapperStyle }>
-  <div style={ style }>
-    <Option /> or <Placeholder />
-    <span style={ caretToCloseStyle or caretToOpenStyle } />
-  </div>
-  <ul style={ menuStyle }>
-    <li>
-      <Option /> or <Separator />
-    </li>
-    <li>
-      <Option /> or <Separator />
-    </li>
-    … more entries …
-  </ul>
-</div>`;
