b6ff4211d862408d61a0e6eda2e847c2df83cff2
Nik Graf <nikgraf.com@gmail.com>
2015-07-15 14:08:23 +0200

    doc(LandingPage): add quote from Vjeux

--- js/Home.jsx
+++ js/Home.jsx
@@ -14,6 +14,32 @@ export default class Home extends Component {
           All of the components are optimized to work both on <b>mobile & desktop devices</b>. The styles are highly customizable on two levels. You can <b>configure the base styles</b> of all the components as well as <b>modify each one of them individually</b>.
         </p>
 
+        <div style={{ marginTop: 40 }}>
+          <p style={{ borderRadius: 2,
+                      clear: 'both',
+                      padding: 5 }}>
+            <span style={{ fontSize: 32, color: '#ccc'}}>“ </span>
+            This is so good. I like the effort you put into tweaking the UX.
+            <span style={{ fontSize: 32,
+                           display: 'inline-block',
+                           position: 'relative',
+                           top: 26,
+                           color: '#ccc',
+                           left: 4 }}> ”</span>
+          </p>
+          <div style={{ marginLeft: 30 }}>
+            <img src="images/vjeux.jpeg"
+                 style={{ height: 50,
+                          'float': 'left',
+                          borderRadius: 50 }} />
+            <div style={{ fontSize: 18,
+                          marginLeft: 60 }}>Christopher Chedeau (Vjeux)</div>
+            <div style={{ color: '#888',
+                          marginLeft: 60 }}>React Core Team</div>
+          </div>
+          <div stlye={{ clear: 'both' }}></div>
+        </div>
+
         <h3>Browser Support</h3>
         <ul>
           <li>Chrome (mobile and desktop)</li>
