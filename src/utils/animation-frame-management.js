01738c15068a86a3646e508c4169ef8eddb05351
jpuri <jyotipuri@gmail.com>
2015-07-15 19:55:54 +0530

    refactor(Utils): adding if condition on window variable to support server side rendering

--- src/utils/animation-frame-management.js
+++ src/utils/animation-frame-management.js
@@ -3,15 +3,18 @@
 export let requestAnimationFrame;
 export let cancelAnimationFrame;
 
-requestAnimationFrame = window.requestAnimationFrame;
-cancelAnimationFrame = window.cancelAnimationFrame;
-
 let lastTime = 0;
-const vendors = ['ms', 'moz', 'webkit', 'o'];
-for (let x = 0; x < vendors.length && !requestAnimationFrame; ++x) {
-  requestAnimationFrame = window[vendors[x] + 'RequestAnimationFrame'];
-  cancelAnimationFrame = window[vendors[x] + 'CancelAnimationFrame'] ||
-                         window[vendors[x] + 'CancelRequestAnimationFrame'];
+
+if(window) {
+  requestAnimationFrame = window.requestAnimationFrame;
+  cancelAnimationFrame = window.cancelAnimationFrame;
+
+  const vendors = ['ms', 'moz', 'webkit', 'o'];
+  for (let x = 0; x < vendors.length && !requestAnimationFrame; ++x) {
+    requestAnimationFrame = window[vendors[x] + 'RequestAnimationFrame'];
+    cancelAnimationFrame = window[vendors[x] + 'CancelAnimationFrame'] ||
+      window[vendors[x] + 'CancelRequestAnimationFrame'];
+  }
 }
 
 if (!requestAnimationFrame) {
