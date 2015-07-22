385978e642f4de6a4ddd9fb640601661c0ae639b
jpuri <jyotipuri@gmail.com>
2015-07-22 12:52:43 +0530

    refactor: writing helper method 'has' in utility.js

---
+++ src/utils/utility.js
@@ -0,0 +1,8 @@
+export function has(obj, key) {
+  return obj != null && Object.prototype.hasOwnProperty.call(obj, key);
+}
+
+export function temp(obj, key) {
+  return obj != null && Object.prototype.hasOwnProperty.call(obj, key);
+}
+
