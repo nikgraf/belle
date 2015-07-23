f6df30671ed01c80bfd2324e537dd435c8304c55
Nik Graf <nikgraf.com@gmail.com>
2015-07-23 16:07:00 +0200

    doc(helpers): add jsdoc documentation

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,7 +1,21 @@
+// The following helper functions are based on Underscore.js
+
+/**
+ * Returns true if the object contain the given key.
+ *
+ * @param {object} obj - object to be inspected
+ * @param {string} key - name of the property
+ */
 export function has(obj, key) {
   return obj !== undefined && obj !== null && Object.prototype.hasOwnProperty.call(obj, key);
 }
 
+/**
+ * Return a copy of the object, filtered to omit the blacklisted keys (or array of keys).
+ *
+ * @param {object} obj - object the returned object is based on
+ * @param {string|string[]} fields - the key or list of keys of the property to omit
+ */
 export function omit(obj, fields) {
   if (obj) {
     const result = {};
@@ -14,6 +28,14 @@ export function omit(obj, fields) {
   }
 }
 
+/**
+ * Looks through each value in the list, returning an array of all the values
+ * that pass a truth test (predicate).
+ *
+ * @param {array} iterable - the iterable object to be filtered
+ * @param {function} predicate - function returning true when provided with an entry as argument
+ * @param {object} [context] - context for the predicate function call
+ */
 export function filter(iterable, predicate, context) {
   if (iterable) {
     const result = [];
@@ -26,18 +48,34 @@ export function filter(iterable, predicate, context) {
   }
 }
 
+/**
+ * Returns true if the provided object is an array.
+ *
+ * @param {object} obj - object to be inspected
+ */
 function isArrayLike(obj) {
   const length = obj.length;
   return typeof length === 'number' && length >= 0;
 }
 
+/**
+ * Returns all the names of the object's properties.
+ *
+ * @param {object} obj - object to be used
+ */
 function keys(obj) {
   const objKeys = [];
   for (const key in obj) if (has(obj, key)) objKeys.push(key);
   return objKeys;
 }
 
-// For both arrays and object - returns array
+/**
+ * Returns a new array of values by mapping each value in list through a transformation function (predicate). If object is a not an array, predicate's arguments will be (value, key, list).
+ *
+ * @param {object|array} obj - object to be based upon
+ * @param {function} predicate - function returning the a new version of the entry
+ * @param {object} [context] - context for the predicate function call
+ */
 export function map(obj, predicate, context) {
   if (obj) {
     const result = [];
@@ -53,7 +91,14 @@ export function map(obj, predicate, context) {
   }
 }
 
-// The function will search only in array(iterable object) and not in plain js object and will return first match
+/**
+ * Returns the first value that passes a truth test (predicate), or undefined if
+ * no value passes the test. Only works for iterable objects e.g. arrays.
+ *
+ * @param {array} iterable - the iterable object to be searched
+ * @param {function} predicate - function returning true in case of a positive match
+ * @param {object} [context] - context for the predicate function call
+ */
 export function find(iterable, predicate, context) {
   if (iterable) {
     let result;
@@ -67,12 +112,23 @@ export function find(iterable, predicate, context) {
   }
 }
 
-// Only for arrays (iterable object)
+/**
+ * Returns true if object contains no values (no enumerable own-properties).
+ *
+ * @param {array} iterable - an iterable object
+ */
 export function isEmpty(iterable) {
   return !iterable || iterable.length === 0;
 }
 
-// The function will search only in array(iterable object) and not in plain js object and will return index for first match
+/**
+ * Returns the index of the first value that passes a truth test (predicate), or undefined if
+ * no value passes the test. Only works for iterable objects e.g. arrays.
+ *
+ * @param {array} iterable - the iterable object to be searched
+ * @param {function} predicate - function returning true in case of a positive match
+ * @param {object} [context] - context for the predicate function call
+ */
 export function findIndex(iterable, predicate, context) {
   if (iterable) {
     let result;
@@ -86,21 +142,33 @@ export function findIndex(iterable, predicate, context) {
   }
 }
 
-// Only for arrays (iterable object)
+/**
+ * Returns the first element of an iterable object.
+ *
+ * @param {array} iterable - must be an iterable object
+ */
 export function first(iterable) {
   if (iterable && iterable.length > 0) {
     return iterable[0];
   }
 }
 
-// Only for arrays (iterable object)
+/**
+ * Returns the last element of an iterable object.
+ *
+ * @param {array} iterable - must be an iterable object
+ */
 export function last(iterable) {
   if (iterable && iterable.length > 0) {
     return iterable[iterable.length - 1];
   }
 }
 
-// Only for arrays (iterable object)
+/**
+ * Return the number of values in the list.
+ *
+ * @param {array} iterable - must be an iterable object
+ */
 export function size(iterable) {
   if (iterable) {
     return iterable.length;
@@ -108,7 +176,13 @@ export function size(iterable) {
   return 0;
 }
 
-// Only for arrays (iterable object)
+/**
+ * Returns true if any of the values in the list pass the predicate truth test.
+ *
+ * @param {array} iterable - iterable object to be searched
+ * @param {function} predicate - function returning true in case of a positive match
+ * @param {object} [context] - context for the predicate function call
+ */
 export function some(iterable, predicate, context) {
   if (iterable) {
     let result;
@@ -122,7 +196,11 @@ export function some(iterable, predicate, context) {
   }
 }
 
-// Only for arrays (iterable object)
+/**
+ * Returns the union of the passed-in arrays: the list of unique items, in order, that are present in one or more of the arrays.
+ *
+ * @param {...array} arrs - at least two iterable objects must be provide
+ */
 export function union(...arrs) {
   if (arrs) {
     const result = [];
@@ -140,11 +218,23 @@ export function union(...arrs) {
 }
 
 let idCounter = 0;
+
+/**
+ * Generate a globally-unique id.
+ *
+ * @param {string} [prefix] - if prefix is passed, the id will be appended to it.
+ */
 export function uniqueId(prefix) {
   const id = ++idCounter + '';
   return prefix ? prefix + id : id;
 }
 
+/**
+ * Copy all of the properties in the source objects over to the destination object, and return the destination object. It's in-order, so the last source will override properties of the same name in previous arguments.
+ *
+ * @param {object} obj1 - object to be extended
+ * @param {...object} objs - at least one but optionally more objects an be provided
+ */
 export function extend(obj1, ...objs) {
   if (obj1 && objs) {
     objs.forEach((obj) => {
@@ -160,6 +250,12 @@ export function extend(obj1, ...objs) {
   return obj1;
 }
 
+/**
+ * Recursive function for flattening an iterable.
+ *
+ * @param {object} output - base object to be updated
+ * @param {object} element - input object to be merged into the output
+ */
 function flattenInternal(output, element) {
   if (element) {
     element.forEach((obj) => {
@@ -172,7 +268,11 @@ function flattenInternal(output, element) {
   }
 }
 
-// Works only for arrays
+/**
+ * Flattens a nested array (the nesting can be to any depth).
+ *
+ * @param {...array} arrays - at least one array must be provided
+ */
 export function flatten(...arrays) {
   if (arrays) {
     const result = [];
