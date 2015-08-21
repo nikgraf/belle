a470921db90688c38ca628984be82ea6ee8259bc
jpuri <jyotipuri@gmail.com>
2015-08-21 14:38:30 +0530

    feat(DatePicker): moving weekend info to locale data

--- src/config/datePicker.js
+++ src/config/datePicker.js
@@ -8,6 +8,7 @@ const datePickerConfig = {
         'juli', 'augustus', 'september', 'oktober', 'november', 'december'],
       dayNamesMin: ['zo', 'ma', 'di', 'wo', 'do', 'vr', 'za'],
       firstDay: 1,
+      weekEnd: 0,
       isRTL: false
     },
     'ar': {
@@ -15,6 +16,7 @@ const datePickerConfig = {
         'يوليو', 'أغسطس', 'سبتمبر', 'أكتوبر', 'نوفمبر', 'ديسمبر'],
       dayNamesMin: ['ح', 'ن', 'ث', 'ر', 'خ', 'ج', 'س'],
       firstDay: 6,
+      weekEnd: 5,
       isRTL: true
     },
     'he': {
@@ -22,6 +24,7 @@ const datePickerConfig = {
         'יולי', 'אוגוסט', 'ספטמבר', 'אוקטובר', 'נובמבר', 'דצמבר'],
       dayNamesMin: ['א\'', 'ב\'', 'ג\'', 'ד\'', 'ה\'', 'ו\'', 'שבת'],
       firstDay: 0,
+      weekEnd: 6,
       isRTL: true
     },
     'fr': {
@@ -29,6 +32,7 @@ const datePickerConfig = {
         'juillet', 'août', 'septembre', 'octobre', 'novembre', 'décembre'],
       dayNamesMin: ['D', 'L', 'M', 'M', 'J', 'V', 'S'],
       firstDay: 1,
+      weekEnd: 0,
       isRTL: false
     },
     'zh-CN': {
@@ -36,6 +40,7 @@ const datePickerConfig = {
         '七月', '八月', '九月', '十月', '十一月', '十二月'],
       dayNamesMin: ['日', '一', '二', '三', '四', '五', '六'],
       firstDay: 1,
+      weekEnd: 0,
       isRTL: false
     }
   }
