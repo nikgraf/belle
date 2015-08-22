427ddf42db2a37f885fc1c84fdae6f0fb5b0de57
jpuri <jyotipuri@gmail.com>
2015-08-22 23:57:45 +0530

    docs(DatePicker): adding config details

--- src/config/datePicker.js
+++ src/config/datePicker.js
@@ -35,7 +35,7 @@ const datePickerConfig = {
       weekEnd: 0,
       isRTL: false
     },
-    zh-CN: {
+    'zh-CN': {
       monthNames: ['一月', '二月', '三月', '四月', '五月', '六月',
         '七月', '八月', '九月', '十月', '十一月', '十二月'],
       dayNamesMin: ['日', '一', '二', '三', '四', '五', '六'],
