7baea35daab59a9f1e61095725b314e088d4ea99
Nik Graf <nik@nikgraf.com>
2015-10-09 13:42:07 +0200

    fix(doc): move back to hash history

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ import ReactDOM from 'react-dom';
 import routes from './components/routes';
 import Router from 'react-router';
 import initializeTheme from './theme/initialize';
-import { createHistory } from 'history';
+import { createHashHistory } from 'history';
 
 // export for http://fb.me/react-devtools
 window.React = React;
 
 initializeTheme();
 
-const history = createHistory();
+const history = createHashHistory();
 const rootComponent = <Router history={history} children={routes} />;
 
 ReactDOM.render(rootComponent, document.getElementById('react'));
