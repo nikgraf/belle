6978c9e2598f6c65d1f46bc4e959cdfd82f038a0
Nik Graf <nik@nikgraf.com>
2015-10-10 23:47:34 +0200

    refactor(Select): remove unused React import

--- src/config/select.js
+++ src/config/select.js
@@ -1,4 +1,3 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
 import isComponentOfType from '../utils/is-component-of-type.js';
 import {filterReactChildren, findIndex} from '../utils/helpers';
