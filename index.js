d3da428d33d103534756a5ab6913c170f07a7627
Nik Graf <nik@nikgraf.com>
2016-03-28 14:32:24 -0700

    fix docs

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import routes from './components/routes';
-import Router, { hashHistory } from 'react-router';
+import { Router, hashHistory } from 'react-router';
 import initializeTheme from './theme/initialize';
 
 // export for http://fb.me/react-devtools
