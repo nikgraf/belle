ce1c7e08f73c2809911f35533402fae19c4a50ad
Tom Conroy <tom@thomasconroy.net>
2017-08-05 13:47:13 -0400

    migrate away from deprecated propTypes to new prop-types package

--- components/Base.js
+++ components/Base.js
@@ -1,4 +1,5 @@
-import React, { PropTypes } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import { Card } from 'belle';
 import { Link } from 'react-router';
 import Column from './Column';
