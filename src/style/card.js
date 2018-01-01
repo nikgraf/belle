d9d28d9a5648f13f261928511a1b39869b5805d2
Manfred Specht <msp@area51.ms>
2018-01-01 19:33:53 +0100

    resize Card-style padding

--- src/style/card.js
+++ src/style/card.js
@@ -2,7 +2,7 @@ export const cardStyle = {
 
   style: {
     marginBottom: 20,
-    padding: 40,
+    padding: 16,
     borderRadius: 2,
     background: '#fff',
     boxShadow: '0 1px 1px rgba(0, 0, 0, 0.2)',
