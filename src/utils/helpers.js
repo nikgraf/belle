87b4f9a0505dd4106579418c4eda8623f7758599
Naveen DA <mailtomassnaveen@gmail.com>
2019-10-10 01:57:46 +0530

    Update uniqueId for ensure the uniqueness

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -384,7 +384,8 @@ export function flattenReactChildren(children) {
 }
 
 export function uniqueId() {
-  return Math.floor((1 + Math.random()) * 0x10000)
-    .toString(16)
-    .substring(1);
+  return 'xxxxxxxx-xxxx-4xxx-yxxx'.replace(/[xy]/g, function(c) {
+    var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+    return v.toString(16);
+  });
 }
